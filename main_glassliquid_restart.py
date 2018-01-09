29078f54cad55d7cb9d7893d3da7a69f99c7e00f
Kirk Swanson <swansonkirk80@gmail.com>
2018-01-09 12:10:28 -0600

    Added -training_data_aug option to match main_glassliquid.py option

--- main_glassliquid_restart.py
+++ main_glassliquid_restart.py
@@ -30,6 +30,7 @@ def create_parser():
 	parser.add_argument('-beta', dest='beta', default=0.01, help='L2 regularization parameter')
 	parser.add_argument('-keep_probability', dest='keep_probability', default=0.5, help='Dropout keep probability')
 	parser.add_argument('-filename', dest='filename', default='test', help='Filename for plots and saved model files')
+	parser.add_argument('-training_data_aug', dest='training_data_aug', default=False, help='Boolean for training dataset augmentation')
 	parser.add_argument('-restart', dest='restart', default="test.ckpt", help='Filename for previous saved model - just use <filename>.ckpt')
 
 	return parser
@@ -47,9 +48,19 @@ def convert_args(args):
 	options['beta'] = args.beta
 	options['keep_probability'] = args.keep_probability
 	options['filename'] = args.filename
+	options['training_data_aug'] = args.training_data_aug
 	options['restart'] = args.restart
 
 	return options
+
+""" Function to convert a string to a boolean for the argparse options"""
+def str2bool(string):
+	if string.lower() == 'true':
+		return True
+	elif string.lower() == 'false':
+		return False
+	else:
+		raise argparse.ArgumentTypeError('Boolean value expected.')
 
 """ Functions to convert the labels to one hot """
 def get_unique_labels(metadata):
@@ -181,7 +192,9 @@ def main(argv):
 	beta = float(options['beta'])
 	keep_probability = float(options['keep_probability'])
 	filename=str(options['filename'])
+	training_data_aug = str2bool(options['training_data_aug'])
 	restart_filename = str(options['restart'])
+
 
 	# Input data
 	x = tf.placeholder(tf.float32, [None, im_size, im_size, 3])
@@ -306,11 +319,11 @@ def main(argv):
 
 				if validation_accuracy < eta_threshold:
 					eta = eta_initial
-					train_X, train_Y = train_generator.next(batch_size, data_aug=False, random_shuffle=True)
+					train_X, train_Y = train_generator.next(batch_size, data_aug=training_data_aug, random_shuffle=True)
 					train_step.run(feed_dict={x: train_X, y_: train_Y, keep_prob: keep_probability, learning_rate: eta})
 				else:
 					eta = eta_final
-					train_X, train_Y = train_generator.next(batch_size, data_aug=False, random_shuffle=True)
+					train_X, train_Y = train_generator.next(batch_size, data_aug=training_data_aug, random_shuffle=True)
 					train_step.run(feed_dict={x: train_X, y_: train_Y, keep_prob: keep_probability, learning_rate: eta})
 				update_tracker += 1
 
